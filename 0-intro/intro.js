48da12e4fefb2f90214d09cb30fd6dd4d6b88714
omardeangelis <omar.hpvfilm@gmail.com>
2023-02-26 18:52:00 +0100

    lezione 1

---
+++ 0-intro/intro.js
@@ -0,0 +1,6 @@
+console.log("benvenuto nel mondo di TypeScript");
+var corso = {
+    titolo: "TypeScript",
+    durata: 24
+};
+corso.lezioni;
---
+++ 0-intro/intro.ts
@@ -0,0 +1,8 @@
+console.log("benvenuto nel mondo di TypeScript")
+
+const corso = {
+  titolo: "TypeScript",
+  durata: 24,
+}
+
+corso.lezioni
